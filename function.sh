1de20feb022f70dca9b49424a082b81936a620ec
Abusaleh Alam Khan <abusaleh.uiu.cse@gmail.com>
2025-12-17 04:52:44 +0600

    Add greet function to display personalized greeting information

---
+++ function.sh
@@ -0,0 +1,12 @@
+# add(a,b){
+
+# }
+
+greet(){
+    echo "Hello $1!"
+    echo "First Name : $2"
+    echo "Middle Name : $3"
+    echo "Last Name : $4"
+    echo "Age : $5"
+}
+greet "World" Maruf Alam Khan 22
