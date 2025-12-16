2dbf43328a2790e58eb92fc5954ea1bc8649beab
Abusaleh Alam Khan <abusaleh.uiu.cse@gmail.com>
2025-12-16 09:57:34 +0600

    show number in list style for-loop with brace expansion

--- for-loop.sh
+++ for-loop.sh
@@ -6,8 +6,14 @@ do
   echo "I like $fruit"
 done  
 
-#loops through files
+#for-loops through files
 for file in *.sh
 do
   echo "Processing : $file"
+done  
+
+#for-loop with number 
+for number in {1..5}
+do
+  echo "number : $number"
 done  
